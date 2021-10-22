349ee73862e564ad4b4f733ba051fcd54314cd4b
lucas-goldner <lucas.goldner@googlemail.com>
2021-10-22 20:40:43 +0200

    feat: Add download button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Download } from './components/Download'
 import { Slideshow } from './components/Slideshow'
 
 export const App = () => {
   return (
     <>
       <Slideshow />
+      <Download />
     </>
   )
 }
---
+++ src/components/Download.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Download.module.css'
+
+export const Download = () => {
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.logo}>Logo</h1>
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Download
+      </a>
+    </section>
+  )
+}
