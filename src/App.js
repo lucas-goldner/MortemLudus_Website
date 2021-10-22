1d9bd67f295995d17f8ebb3009f49c864d532638
lucas-goldner <lucas.goldner@googlemail.com>
2021-10-22 20:51:50 +0200

    feat: Add date

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Slideshow } from './components/Slideshow'
 export const App = () => {
   return (
     <>
+      <div style={{ position: 'absolute', top: 10, right: 40, fontSize: '13px' }}>
+        {new Date().getMonth() + '/' + new Date().getDay() + '/' + new Date().getFullYear()}
+      </div>
       <Slideshow />
       <Download />
     </>
