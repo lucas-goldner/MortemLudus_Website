46f71b752f7f8d81fd1ad106ff96b7fdb18ff674
lucas-goldner <lucas.goldner@googlemail.com>
2021-10-23 02:11:49 +0200

    feat: Add hover to newsarticles

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -3,7 +3,7 @@ import styles from '../styles/Newsarticle.module.css'
 export const NewsArticle = ({ title, descr, img }) => {
   return (
     <article className={styles.articleCon}>
-      <img src={img} />
+      <img className={styles.img} src={img} />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.descr}>{descr}</p>
     </article>
